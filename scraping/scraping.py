62d737db14a15332e37f2461c2fa9e1f12799a98
homeseeker88 <sadkowskim@yahoo.pl>
2025-03-26 00:19:09 +0100

    retrieving countries, cities and links from html page

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -2,15 +2,25 @@ import requests
 from bs4 import BeautifulSoup, ResultSet
 
 
-def read_homologations() -> ResultSet:
+def read_homologations() -> tuple[ResultSet, ResultSet, ResultSet]:
     r = requests.get("https://www.fis-ski.com/DB/ski-jumping/homologations.html")
     soup = BeautifulSoup(r.content, "html.parser")
     soup.prettify()
     s = soup.find_all("div", class_="table-row pointer reset-padding")
-    print(soup.get_text())
+    cities = soup.find_all("div", class_ = "g-xs g-sm g-md g-lg justify-left bold")
+    countries = soup.find_all("span", class_="country__name-short")
+    print(s)
+    rows = soup.find_all("div", class_="table-row pointer reset-padding")
+    return cities,countries,rows
 
-    return s
+
+def prepare_data(cities: list[str], country: list[str], urls: list[str]) -> None:
+    #TODO
 
 
 if __name__ == "__main__":
-    content = read_homologations()
+    cities,countries, rows = read_homologations()
+    for city, country, row in zip(cities, countries, rows):
+        print(city.getText(),country.getText(), row["onclick"], type(row["onclick"]))
+
+
